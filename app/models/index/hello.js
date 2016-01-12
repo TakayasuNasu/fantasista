a27fc8b4835e70012cf73714338e8046383d8fe0
stb-nasu <nasu@strawbag.co.jp>
2016-01-12 01:25:06 +0000

    deleted hello model

--- app/models/index/hello.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOMServer from 'react-dom/server';
-import CommentBox from "../../client/components/comment";
-
-export default class Hello {
-  getContent() {
-    return ReactDOMServer.renderToString(<CommentBox />);
-  }
-}
